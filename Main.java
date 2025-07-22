3a8ad7e0c166e503e4c547b7b38f7607cf48cef5
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-22 22:35:30 +0400

    Create Objects Add list for account, newAccount method, and viewAccountDetails method

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static List<BankAccount> accounts = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
@@ -8,10 +11,10 @@ public class Main {
             displayMenu();
             String option = scanner.next();
             switch (option) {
-                case "1": break;
+                case "1": newAccount(scanner); break;
                 case "2": break;
                 case "3": break;
-                case "4": break;
+                case "4": viewAccountDetails(scanner); break;
                 case "5": exit = true; System.out.println("\nGoodbye!");
                     scanner.close();
                     break;
@@ -30,4 +33,43 @@ public class Main {
                 
                 Please enter your option:""" + " ");
     }
+
+    public static void newAccount(Scanner scanner) {
+        System.out.print("Enter account name: ");
+        scanner.nextLine(); // Consume leftover newline
+        String name = scanner.nextLine();
+
+        System.out.print("Enter account number: ");
+        String number = scanner.nextLine();
+
+        System.out.print("Enter initial balance: ");
+        double balance = scanner.nextDouble();
+
+        BankAccount account = new BankAccount(name, number, balance);
+        accounts.add(account);
+
+        System.out.println("\nAccount created successfully!\n");
+    }
+
+    public static void viewAccountDetails(Scanner scanner) {
+        if (accounts.isEmpty()) {
+            System.out.println("No accounts found.\n");
+            return;
+        }
+
+        scanner.nextLine(); // clear buffer
+        System.out.print("Enter account number: ");
+        String inputNumber = scanner.nextLine();
+
+        for (BankAccount account : accounts) {
+            if (account.getAccountNumber().equals(inputNumber)) {
+                System.out.println("\nAccount details:");
+                account.showAccountInfo();
+                return;
+            }
+        }
+        System.out.println("Account not found.\n");
+    }
+
+
 }

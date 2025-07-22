f6ffd88da9959be79d184870ccc1cf835d739ec9
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-22 22:06:43 +0400

    Update showAccountInfo()

--- BankAccount.java
+++ BankAccount.java
@@ -12,6 +12,6 @@ public class BankAccount {
     public void showAccountInfo() {
         System.out.println("Account Name: " + this.accountName);
         System.out.println("Account Number: " + this.accountNumber);
-        System.out.println("Account Name: " + this.balance);
+        System.out.println("Balance: " + this.balance);
     }
 }

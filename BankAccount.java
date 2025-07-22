12ebfe19d7e2236ba2988702dd6b504746e08d2e
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-22 09:06:52 +0400

    Create BankAccount class

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+    private String accountName;
+    private String accountNumber;
+    private double balance;
+
+    public void showAccountInfo() {
+        System.out.println("Account Name: " + this.accountName);
+        System.out.println("Account Number: " + this.accountNumber);
+        System.out.println("Account Name: " + this.balance);
+    }
+}

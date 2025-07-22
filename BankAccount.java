1a69f04d585f0b50ce85af795866107d6bcf5f5b
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-22 09:10:12 +0400

    feat: Add a Constructor

--- BankAccount.java
+++ BankAccount.java
@@ -3,6 +3,12 @@ public class BankAccount {
     private String accountNumber;
     private double balance;
 
+    public BankAccount(String accountName, String accountNumber, double balance) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
     public void showAccountInfo() {
         System.out.println("Account Name: " + this.accountName);
         System.out.println("Account Number: " + this.accountNumber);

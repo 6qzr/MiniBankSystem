4660066f120301e90e278b89209f47835388cc58
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-23 08:23:17 +0400

    Add Withdrow and Deposit to BankAccount

--- BankAccount.java
+++ BankAccount.java
@@ -21,6 +21,28 @@ public class BankAccount {
         return balance;
     }
 
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Deposit amount must be positive.");
+            return;
+        }
+        balance += amount;
+        System.out.println("Deposit successful. New balance: " + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Withdraw amount must be positive.");
+            return;
+        }
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + balance);
+        } else {
+            System.out.println("Not enough balance.");
+        }
+    }
+
     public void showAccountInfo() {
         System.out.println("Account Name: " + this.accountName);
         System.out.println("Account Number: " + this.accountNumber);
